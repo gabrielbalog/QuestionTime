c902430b7f5d0e396a43a75c6fd468b6dccc3fd7
Gabriel Balog <gabrielbalog68@gmail.com>
2019-11-18 18:52:26 -0300

    API para responder e visualizar Answers

--- questions/api/serializers.py
+++ questions/api/serializers.py
@@ -10,7 +10,7 @@ class AnswerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Answer
-        exclude = ('question', 'voters', 'update_at')
+        exclude = ('question', 'voters', 'updated_at')
 
     def get_created_at(self, instance):
         return instance.created_at.strftime("%B %d %Y")
@@ -35,7 +35,7 @@ class QuestionSerializer(serializers.ModelSerializer):
         exclude = ('updated_at',)
 
     def get_created_at(self, instance):
-        return instance.created_at.strftime("%B %d %Y")
+        return instance.created_at.strftime("%B %d, %Y")
 
     def get_answers_count(self, instance):
         return instance.answers.count()
